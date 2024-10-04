600cc8b0c86e7bb3b957e6cea398397212f9af03
Dawid Wysocki <dwysocki@google.com>
2024-10-04 08:38:59 +0000

    print numbers from an array

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
+	arr := []int{1, 2, 3}
+
+	for _, num := range arr {
+		if _, err := fmt.Println("num:", num); err != nil {
+			err := errors.Join(errors.New("couldn't print"), err)
+			panic(err)
+		}
+	}
+
 	fmt.Println("hello from the other side")
 }
